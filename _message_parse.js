de9f152087aaa5d16aa31f6e1a7d9d964f7a89ff
Olympicene <kevinjcord@gmail.com>
2021-05-18 18:29:20 -0500

    added wipe/add command arguments

--- _message_parse.js
+++ _message_parse.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const databaseDir = path.resolve(__dirname + "/database/");
 
-
+fs.writeFile('training_data.txt', '', function(){console.log('wiped training_data.txt' + '\n')})
 const length = fs.readdirSync(databaseDir).length
 
 var wrap = ''
@@ -16,7 +16,7 @@ for (var i = 1; i <= length; i++) {
     for (var message in file.messages) {
         if ('content' in file.messages[message] && 'reactions' in file.messages[message]) {
             if (file.messages[message].reactions.length > 2) {
-                data += '\'' + file.messages[message].content + '\'' + '\n'
+                data += '\'' + cleanString(file.messages[message].content) + '\'' + '\n'
             }
         }
     }
@@ -24,7 +24,17 @@ for (var i = 1; i <= length; i++) {
     wrap += data;
 }
 
-fs.writeFile('Output.txt', wrap, (err) => {
+fs.writeFile('training_data.txt', wrap, (err) => {
     if (err) return console.error(err);
 });
+
+function cleanString(input) {
+    var output = "";
+    for (var i=0; i<input.length; i++) {
+        if (input.charCodeAt(i) <= 127) {
+            output += input.charAt(i);
+        }
+    }
+    return output;
+}
 
--- json_convert.js
+++ json_convert.js
@@ -2,24 +2,39 @@ const lineReader = require('line-reader');
 const fs = require("fs");
 var stringSimilarity = require("string-similarity");
 
+var args = process.argv.slice(2);
 
-fs.writeFile('output.json', '[]', function(){console.log('wiped output.txt \n')})
+switch (args[0]) {
+    case 'add':
+        console.log('adding to gpt.json');
+        break;
+    case 'wipe':
+        fs.writeFile('gpt.json', '[]', function(){console.log('wiped gpt.json \n')})
+        break;
+    default:
+        console.log('adding to gpt.json');
+    }
 
 
 var prev_text = ''
 
 lineReader.eachLine('input.txt', (line) => { 
 
-    var text = line.substring(1, line.length-1)
-    
-    if (stringSimilarity.compareTwoStrings(text, prev_text) < 0.9) {
+    if (line[0] == '\'' && line[line.length-1] == '\'' && line.split(" ").length > 2) {
 
-        var messages = JSON.parse(fs.readFileSync('output.json'))   
-    
-        messages.push(text)
-    
-        fs.writeFileSync('output.json', JSON.stringify(messages, null, '\t'));
+        var text = line.substring(1, line.length-1);
+
+        if (stringSimilarity.compareTwoStrings(text, prev_text) < 0.9 && text.length > 1) {
+
+            var messages = JSON.parse(fs.readFileSync('gpt.json'))   
+        
+            messages.push(text)
+        
+            fs.writeFileSync('gpt.json', JSON.stringify(messages, null, '\t'));
+        }
+
+        prev_text = text;
+
     }
 
-    prev_text = text;
 });
